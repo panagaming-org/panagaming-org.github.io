ec447a9eb0bdf87c752c94db2f3b475977441443
Xx-IKERO-90-xX <ikerjrh2003@gmail.com>
2026-01-24 13:01:28 +0100

    Mejoramos el background de la aplicacion

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
 
 <style>
   .panagaming-section {
-    background-image: url(../src/assets/images/backgrounds/exagons-green-neon.png);
+    background-image: 
+      linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+      url(../src/assets/images/backgrounds/exagons-green-neon.png);
     background-repeat: repeat;
     background-attachment: fixed;
     background-size: 1600px;
